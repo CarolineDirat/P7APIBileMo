bafd4e5e4e3ea42d1280c76f03d613e0d03e98bb
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-10-12 15:54:52 +0200

    create UserByClientController::item() to get the details of one user linked by a client, identified by his uuid : GET /api/clients/{client_uuid}/users/{user_uuid}

--- src/Controller/UserByClientController.php
+++ src/Controller/UserByClientController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * UserByClientController.
@@ -36,8 +39,49 @@ class UserByClientController extends AbstractController
             $serializer->serialize(
                 $users,
                 'json',
-                ['groups' => 'collection']
+                ['groups' => 'get']
             ),
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    /**
+     * item
+     * Get details of one user linked by a client
+     * 
+     * @Route(
+     *  "/{user_uuid}",
+     *  name="item_get",
+     *  methods={"GET"},
+     *  stateless=true
+     * )
+     * 
+     * @ParamConverter("user", options={"mapping": {"user_uuid": "uuid"}})
+     * 
+     * @param Client $client
+     * @param User $user
+     * @param SerializerInterface $serializer
+     *
+     * @return JsonResponse
+     */
+    public function item(
+        Client $client,
+        User $user,
+        SerializerInterface $serializer
+    ): JsonResponse {
+
+        if ($user->getClient() !== $client) {
+            throw new AccessDeniedHttpException(
+                "You cannot access to the user by this client.",
+                null,
+                Response::HTTP_FORBIDDEN
+            );
+        }
+
+        return new JsonResponse(
+            $serializer->serialize($user, 'json', ['groups' => "get"]),
             Response::HTTP_OK,
             [],
             true
