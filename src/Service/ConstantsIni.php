cfab3d994795bac70ea80f993b8355f009c36157
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-10-12 18:50:15 +0200

    create src/Service/ConstantsIni.php to acces to the constants.ini file containing constants for the application

---
+++ src/Service/ConstantsIni.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class ConstantsIni
+{
+    private ParameterBagInterface $parameterBag;
+    
+    /**
+     * constantsIni
+     * Array of constants contained in the constants.ini file.
+     *
+     * @var array<string, mixed>
+     */
+    private array $constantsIni;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+        $this->constantsIni = parse_ini_file(
+            $this->parameterBag->get('app.constants_ini'),
+            true,
+            INI_SCANNER_TYPED
+        );
+    }
+    
+    /**
+     * getConstantsIni
+     *
+     * @return array<string, mixed>
+     */
+    public function getConstantsIni(): array
+    {
+        return $this->constantsIni;
+    }
+}
